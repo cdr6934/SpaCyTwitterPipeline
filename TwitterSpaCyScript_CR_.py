48a531c9602b499449bf4e23eb40f837af65856b
Chris Ried <chris@cbayes.com>
2018-06-17 20:36:42 -0500

    Added ignore

--- TwitterSpaCyScript_CR_.py
+++ TwitterSpaCyScript_CR_.py
@@ -5,15 +5,12 @@ from tweepy import OAuthHandler
 from tweepy import Stream
 import json
 import spacy
+import keys
 
 nlp = spacy.load('en_core_web_sm')
-consumer_key = "QDrsiY9CHd5MgWxTtJB7BUJ9K"
-consumer_secret = "dnllP0FFBlbFGdV29h6dJH4Ov9q3Nr3EOFuyaikvEstPlKoLk1"
-access_token = "6627802-UMhQ6mesD8pDiCMUVDGXsk3hO3ct03KXtVPpMT7lSP"
-access_token_secret = "NayOyGdPmHzfMceVDD5ZoXlk500DoNXRQT4tG3QuTpCj0"
 
-auth = OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+auth = OAuthHandler(keys.consumer_key, keys.consumer_secret)
+auth.set_access_token(keys.access_token, keys.access_token_secret)
 
 #This is a basic listener that just prints received tweets to stdout.
 class StdOutListener(StreamListener):
@@ -22,8 +19,12 @@ class StdOutListener(StreamListener):
         parsed_json = json.loads(data)
         doc = nlp(parsed_json['text'])
         # Find named entities, phrases and concepts
-        for entity in doc.ents:
-            print(entity.text, entity.label_)
+        #for entity in doc.ents:
+        #    print(entity.text, entity.label_)
+
+        for token in doc:
+            print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_,
+                  token.shape_, token.is_alpha, token.is_stop)
 
         #print(parsed_json['text'])
         return True
@@ -35,8 +36,8 @@ if __name__ == '__main__':
 
     #This handles Twitter authetification and the connection to Twitter Streaming API
     l = StdOutListener()
-    auth = OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
+    auth = OAuthHandler(keys.consumer_key, keys.consumer_secret)
+    auth.set_access_token(keys.access_token, keys.access_token_secret)
     stream = Stream(auth, l)
 
     #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
