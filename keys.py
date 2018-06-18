b53634dadd1b96c510fba13e4dcb117a51ec13c0
Christopher Ried <digitalbro@gmail.com>
2018-06-18 14:08:26 -0500

    Delete keys.py

--- keys.py
+++
@@ -1,5 +0,0 @@
-# Following are the Twitter Keys
-consumer_key = "3bSHBtCfdtSZPIaSg4tCvwFZ5"
-consumer_secret = "X3EbuvGacEmTXNPvJYXZgYr2U10AJmXSdy6P6xsYbD0jBsqVgh"
-access_token = "6627802-UMhQ6mesD8pDiCMUVDGXsk3hO3ct03KXtVPpMT7lSP"
-access_token_secret = "NayOyGdPmHzfMceVDD5ZoXlk500DoNXRQT4tG3QuTpCj0"
